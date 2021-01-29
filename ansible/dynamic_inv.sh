84e720dcc3da3d68f18cab11a788d11784d1898c
Alexey Bichutsky <abichutsky@gmail.com>
2021-01-29 23:36:23 +0300

    hw8 (all tasks) is done

---
+++ ansible/dynamic_inv.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+# находим публичные IP-адреса хостов в YC
+appserver=$(yc compute instance list | grep "reddit-app-0" | awk '{print $10}')
+dbserver=$(yc compute instance list | grep "reddit-db-dev1" | awk '{print $10}')
+
+if [ "$1" == "--list" ]; then
+
+cat<< EOF
+{
+  "app": {
+    "hosts": [
+      "$appserver"
+   ],
+   "vars": {
+      "example_var": "value"
+   }
+  },
+  "db": {
+    "hosts": [
+      "$dbserver" 
+   ],
+   "vars": {
+      "example_var": "values"
+   }
+  },
+  "_meta": {
+    "hostvars": {}
+    }
+}
+EOF
+elif [ "$1" == "--host" ]; then
+  echo '{"_meta": {hostvars": {}}}'
+else
+  echo "{ }"
+fi
+
